7013240456858038a4037b6eac4397500aadb88e
Ambat Vipin <ambat.vipu@gmail.com>
2022-11-03 09:09:24 +0530

    Add files via upload
    Get and Put char demo

---
+++ GetCharPutCharDemo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a,b;
+    
+    printf("Enter two characters\n");
+    a = getchar();
+    b = getchar();
+    printf(" Entered charcters are as follows \n");
+    putchar(a);
+    putchar(b);
+    
+    
+    
+
+}
