ac760a1f9ca5905a0c2bab2baba4b42d85616b02
Ambat Vipin <ambat.vipu@gmail.com>
2022-11-11 10:04:57 +0530

    Input string using scanf

---
+++ StringDemoScanf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    puts("Get a string and print it using array");
+    
+    char name[20];
+    
+    puts("Enter the string");
+    
+    scanf("%s",name); // Enter text without space
+    
+    printf("Enetered string is %s",name);
+   
+    
+}
