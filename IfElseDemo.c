8bb02dedec0c9e1fd567b3c31cd067c80d1a1fbc
Ambat Vipin <ambat.vipu@gmail.com>
2022-11-11 09:46:36 +0530

    Program for normal IF Else condition

---
+++ IfElseDemo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int a;
+    
+    printf("Verify if the number is greater than 10\n");
+    printf("Enter the number");
+    scanf("%d",&a);
+    if(a>10)
+    {
+        printf("The number is greather than 10");
+    }
+    else
+    {
+        printf("The number is less than 10");
+    }
+   
+    
+}
