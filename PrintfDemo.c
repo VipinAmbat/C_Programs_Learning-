a117d2364c4f1549cb75b348995cd5a0fa5a6061
Ambat Vipin <ambat.vipu@gmail.com>
2023-06-01 11:31:03 +0530

    Add files via upload
    Printf Demo

---
+++ PrintfDemo.c
@@ -0,0 +1,11 @@
+/* A program to use printf with newline escape charcater */
+#include<stdio.h>
+
+int main()
+{
+printf(" A new learning\n"); // Added a new line
+printf(" by Ambat vipin");
+printf(" lets go baby \n At Bratislava");
+
+return(0);
+}
