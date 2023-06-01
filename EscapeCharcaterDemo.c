38f124146871969d0714b0588ad40c69bcf1460d
Ambat Vipin <ambat.vipu@gmail.com>
2023-06-01 11:53:26 +0530

    Add files via upload
    Escape character demo

---
+++ EscapeCharcaterDemo.c
@@ -0,0 +1,17 @@
+/* Escape character demo */
+
+#include<stdio.h>
+
+int main()
+{
+
+puts("Newline and backslash use");
+printf(" Hello \n Dan \\Hope your are able to hear me\n"); //newline and backslash character
+puts("Tab character");
+printf("\n Hello Dan \tHope your are able to hear me\n"); //tab character
+puts("Escape single  charcater");
+printf("\n Hello Dan \'Hope you are good \n"); // Escape single quote
+puts("Escape double quote");
+printf("\n Hello Dan \"Hope you are good"); //Escape double quote
+return(0);
+}
